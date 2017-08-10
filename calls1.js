5ce307a753877a7cbc3766fbe5fc6dc3802ef760
Trent Bennett <rehab.rtb@gmail.com>
2017-08-10 12:46:43 -0600

    changes

--- calls1.js
+++ calls1.js
@@ -146,3 +146,141 @@
 //   return evenList;
 // }
 // console.log(evenFinder(nums));
+
+//REDUCE PROBLEM
+/*Using reduce get the total of the following array*/
+// var totalArr = [1,2,3,4,5,6,7,10,20,50];
+// var a = totalArr.reduce(function (sum,current){
+//   return sum + current;
+// }, 0);
+//
+// console.log(a);
+//
+// /*Using reduce calculate the total of only the negative numbers of this array*/
+// var totalArr = [1,2,-3,4,-5,6,7,-10,-20,50];
+// var b = totalNegArr.reduce(function(sum, current){
+//   if (current < 0) {
+//     return sum + current;
+//   }
+//   else {
+//     return sum;
+//   }
+// },0);
+// console.log(b);
+//
+// /* Reduce the following nested array to a flat array containing all the values*/
+// var nestedArr = [[1,2],[3,4],[5,6]];
+// nestedArr.reduce(function(flatArr, current){
+//   return flatArr.concat(current);
+// },[]);
+//
+// //MAP PROBLEM
+// /*Use map to create a new array of doubled values of numArr*/
+// var numArr = [1,2,3,4,5];
+// numArr.map(function(element){
+//   return element * 2;
+// });
+//
+// //MAP PROBLEM
+// /*Use map to create an array of just id's from the following array*/
+// var objArr = [
+//   {id: 1, name: "Dan"},
+//   {id: 2, name: "Fred"},
+//   {id: 3, name: "Steve"},
+//   {id: 4, name: "Greg"}
+// ];
+//
+// objArr.map(function(obj){
+//   return obj.id;
+// })
+//
+// //FILTER PROBLEM
+// /*Create a new arr using filter without the falsey values from the following array*/
+// var arr = [1, 0, true, {}, false, null, "a string", ""];
+// var z = arr.filter(function(element){
+//   return element;
+// });
+//
+// console.log(z);
+//
+// /*Create a new arr filtering out any strings from keepArr that are found in removeArr*/
+// var keepArr = ["string", "anotherstring", "whataboutthis", "heyo"];
+// var removeArr = ["anotherstring", "heyo"];
+//
+// keepArr.filter(function(element){
+//   return removeArr.indexOf(element) === -1;
+// });
+//
+// var obj = {
+//   first: 2,
+//   second: 1,
+//   third: 25,
+// };
+
+
+// function largeNumbers({first, second, third}){
+//   if (first.length < second.length && first.length < third.length){
+//     return first;
+//   }
+//   else if (second.length < third.length){
+//     return second;
+//   }
+//   else {
+//     return third;
+//   }
+// };
+// console.log(largeNumbers(obj));
+
+// var employees = [{
+//   "firstName": "Von",
+//   "lastName": "Budibent",
+//   "email": "vbudibent0@163.com",
+//   "department": "Sales"
+// }, {
+//   "firstName": "Catherina",
+//   "lastName": "Swalowe",
+//   "email": "cswalowe1@example.com",
+//   "department": "Engineering"
+// }, {
+//   "firstName": "Theo",
+//   "lastName": "Trill",
+//   "email": "ttrill2@sina.com.cn",
+//   "department": "Services"
+// }, {
+//   "firstName": "Elsy",
+//   "lastName": "McCrorie",
+//   "email": "emccrorie3@netscape.com",
+//   "department": "Legal"
+// }, {
+//   "firstName": "Lorie",
+//   "lastName": "Handsheart",
+//   "email": "lhandsheart4@fotki.com",
+//   "department": "Research and Development"
+// }]
+// // Create a function called 'employeeUpdater'. employeeUpdater will loop over the array above and perform the following:
+// //  1. If employee's first name is Theo, remove that employee because he just got fired.
+// //  2. If the employee's first name is Lorie, change her department to 'HR'.
+// //  3. Return the updated employee array.
+//
+// function employeeUpdater(arr){
+//   if (employees.firstName === 'Theo');
+//   delete employees[1];
+// }
+// }
+//
+// console.log(employeeUpdater(employees));
+
+const mixedNumbers = [6,3,1,7,5,2,6,8,9,4,2,7,9,3,1,8,4,3];
+
+let evenNumbers = mixedNumbers.filter(function(element){
+  return element % 2 === 0;
+});
+
+console.log(evenNumbers);
+
+const prices = [15.00, 23.00, 78.00, 34.00, 12.00, 86.00, 12.00, 79.00, 32.00];
+
+let postTaxPrices = prices.map(function(element){
+  return element * 1.07;
+});
+console.log(prices);
